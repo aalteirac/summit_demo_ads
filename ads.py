73e726a96be441d96658c6e1e42ab41cfce18281
Anthony Alteirac <105443206+sfc-gh-aalteirac@users.noreply.github.com>
2023-05-14 15:17:16 +0200

    2 tabs completed

--- ads.py
+++ ads.py
@@ -8,6 +8,31 @@ from st_aggrid import AgGrid, GridOptionsBuilder, JsCode,GridUpdateMode
 session=None
 
 @st.cache_data(show_spinner=False,ttl=5000)
+def getDistinctAdvertisers():
+    df=session.sql(f'''
+    select distinct ADVERTISER_NAME from SUMMIT_JIM_DB.RAW_SC."CLICKS";
+    ''').collect()
+    return df
+
+def getAdvertiserData(adv):
+    df=session.sql(f'''
+    select *,1 as IMPRESSIONS,
+    to_date(TO_VARCHAR(to_date(to_timestamp(time_ts/1000000)), 'yyyy-MM-01')) as MONTH,
+    to_date(to_timestamp(time_ts/1000000)) as DATE_IMP from SUMMIT_JIM_DB.RAW_SC."IMPRESSIONS" 
+    WHERE ADVERTISER_NAME='{adv}';
+    ''').collect()
+    return pd.DataFrame(df)
+
+def getClickDataByAdvertiser(adv):
+    df=session.sql(f'''
+    select *, 1 as CLICKS,
+    to_date(TO_VARCHAR(to_date(to_timestamp(time_ts/1000000)), 'yyyy-MM-01')) as MONTH,
+    to_date(to_timestamp(time_ts/1000000)) as DATE_IMP from SUMMIT_JIM_DB.RAW_SC."CLICKS" 
+    WHERE ADVERTISER_NAME='{adv}';
+    ''').collect()
+    return pd.DataFrame(df)  
+
+@st.cache_data(show_spinner=False,ttl=5000)
 def getRawCampaign():
     queryAll=f'''
     SELECT *,to_date(TO_VARCHAR(DAY, 'yyyy-MM-01')) as MONTH,coalesce((clicks/NULLIF(impressions,0))*100,0) as CTR,coalesce(((clicks + likes + shares)/NULLIF(impressions,0))*100,0) as ER from adverity.adverity."Marketing_Data";
@@ -19,27 +44,73 @@ def getRawCampaign():
                                       'CLICKS':'sum',
                                       'CTR':"mean"}).reset_index()
 
-def getCTRByGenderByAge(df):
-    return df.groupby(['GENDER','AGE_RANGE']).agg({'CTR':'mean'}).reset_index()
+def getCTRByDevice(df):
+    mt=df.groupby(['DEVICECATEGORY']).agg({'IMPRESSIONS':'sum',
+            'CLICKS':'sum'})
+    mt['CTR']=(mt['CLICKS']/mt['IMPRESSIONS'] )*100       
+    return mt.reset_index()
+
+def getTopBottomAds(df,bottom=True,n=3):
+    mt=df.groupby(['ORDERNAME']).agg({'IMPRESSIONS':'sum',
+            'CLICKS':'sum'})
+    mt['CTR']=(mt['CLICKS']/mt['IMPRESSIONS'] )*100       
+    return mt.sort_values(by=['CTR'],ascending=bottom).reset_index().head(n)
+
+def getChartTopAds(df,asc=True,prefix='Top'):
+    df=df.sort_values(by=['CTR'],ascending=asc)
+    fig = go.Figure(data=[
+        go.Bar(name='IMPRESSIONS', y=df['ORDERNAME'], x=df['CTR'],text=df['CTR']/100, orientation='h')
+    ])
+    fig.update_xaxes(visible=False, showticklabels=False)
+    fig.data[0].marker.color = ('blue','green','darkgrey')
+    fig.update_layout(margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=90,
+            pad=4
+        ))
+    fig.update_traces(texttemplate='%{text:.2%}', textposition='inside')
+    fig.update_layout(height=490,title=prefix+' Performing Campaigns by CTR(%)') #yaxis_range=[1.2,1.25]
+    config = {
+        "displayModeBar": False
+    }
+    st.plotly_chart(fig,config=config, theme="streamlit",use_container_width=True)    
+
+def getChartCTRByDevice(df):
+    print(df)
+    fig = go.Figure(data=[
+        go.Bar(name='IMPRESSIONS', x=df['DEVICECATEGORY'], y=df['CTR'],yaxis='y',text=df['CTR']/100)
+    ],layout={
+        'yaxis': {'title': 'CTR(%)','showgrid':False,'showline':False}
+    })
+    fig.data[0].marker.color = ('blue','green','darkgrey')
+    fig.update_traces(texttemplate='%{text:.2%}', textposition='inside')
+    fig.update_layout(height=540,title='CTR(%) by Device Type',yaxis_range=[df['CTR'].min() - (df['CTR'].min()/50),df['CTR'].max()]) #yaxis_range=[1.2,1.25]
+    st.plotly_chart(fig, theme="streamlit",use_container_width=True)
 
 def getKPIByCampaignAds(df):
-    df=df.groupby(['COUNTRY_NAME', 'CAMPAIGN','AD_TYPE', 'AD_NAME']).agg({
-                        'VIDEO_COMPLETIONS':'sum',
-                        'VIEWS':'sum',
-                        'COSTS':'sum',
-                        'CTR':"mean",
+    df=df.groupby(['ORDERNAME','LINE_ITEM']).agg({
                         'IMPRESSIONS':"sum",
-                        'CLICKS':"sum"
+                        'CLICKS':"sum",
+                        'SELLERRESERVEPRICE':'sum'
                         }).reset_index()
-    df['CPVMANUAL']=0
-    df['CPCVMANUAL']=0
-    return df[['CAMPAIGN','AD_TYPE','AD_NAME','IMPRESSIONS', 'CLICKS','CTR','CPVMANUAL','CPCVMANUAL','COSTS','VIDEO_COMPLETIONS','VIEWS']].sort_values(['CAMPAIGN'])
+    df['CTR']=(df['CLICKS']/df['IMPRESSIONS'] )*100                     
+    return df[['ORDERNAME','LINE_ITEM','IMPRESSIONS', 'CLICKS','CTR','SELLERRESERVEPRICE']].sort_values(['ORDERNAME'])
 
-def getCTRByDevice(df):
-    return df.groupby(['DEVICE_TYPE']).agg({'CTR':'mean'}).reset_index()
+def getDollarRenderer():
+    rd = JsCode('''
+        function(params) { 
+            return '<span>$' + parseFloat(params.value).toFixed(2) + '</span>'}
+    ''') 
+    return rd  
 
-def getTopBottomAds(df,bottom=False,n=3):
-    return df.groupby(['AD_NAME']).agg({'ER':'mean'}).sort_values(by=['ER'],ascending=bottom).reset_index().head(n)
+def getNbenderer():
+    rd = JsCode('''
+        function(params) { 
+            return '<span>' + parseFloat(params.value).toLocaleString('en-US', {maximumFractionDigits:2}) + '</span>'}
+    ''') 
+    return rd  
 
 def getPercentRenderer():
 
@@ -48,56 +119,65 @@ def getPercentRenderer():
     ''') 
     return rd   
 
-def getChartCTRByDevice(df):
-    print(df)
-    fig = go.Figure(data=[
-        go.Bar(name='IMPRESSIONS', x=df.index, y=df['CTR'],yaxis='y',text=df['CTR']/100)
-    ],layout={
-        'yaxis': {'title': 'CTR(%)','showgrid':False,'showline':False}
-    })
-    fig.data[0].marker.color = ('blue','green','darkgrey')
-    fig.update_traces(texttemplate='%{text:.2%}', textposition='inside')
+def customAggCPC():
+    rd=JsCode('''
+    function(params) {
+                 if ((params.node.data && params.node.data.CLICKS==0) || (params.node.aggData && params.node.aggData.CLICKS==0)){
+                    return 0;
+                }
+                if (params.node.data){
+                    return params.node.data.SELLERRESERVEPRICE/params.node.data.CLICKS;
+                }
+                return params.node.aggData.SELLERRESERVEPRICE/params.node.aggData.CLICKS;
+            }
+    ''')
+    return rd
 
-    no_color = "rgba(128, 0, 128, 0.7)"
-    shapes = []
-    h=0.5
-    for x, y in zip(fig["data"][0]["x"], fig["data"][0]["y"]):
-        rounded_top_right = f' L {x+h}, {y} Q {x}, {y} {x}, {y+h}Z'
-        print(rounded_top_right)
-        shapes.append(
-            dict(type="path",layer='above', path=rounded_top_right, line_color=no_color, fillcolor='red',line=dict(color='rgba(128, 0, 128, 0.7)', width=5.5))
-        )
-    fig.update_layout(margin=dict(
-            l=0,
-            r=0,
-            b=0,
-            t=30,
-            pad=4
-        )
-        )
-    fig.update_layout(height=490,title='CTR(%) by Device Type',yaxis_range=[df['CTR'].min() - (df['CTR'].min()/50),df['CTR'].max()],shapes=shapes) #yaxis_range=[1.2,1.25]
-    st.plotly_chart(fig, theme="streamlit",use_container_width=True)
-
-def getChartTopAds(df,asc=True,prefix='Top'):
-    df=df.sort_values(['ER'], ascending=[asc])
-    fig = go.Figure(data=[
-        go.Bar(name='IMPRESSIONS', y=df['AD_NAME'], x=df['ER'],text=df['ER']/100, orientation='h')
-    ])
-    fig.update_xaxes(visible=False, showticklabels=False)
-    fig.data[0].marker.color = ('blue','green','darkgrey')
-    fig.update_layout(margin=dict(
-            l=0,
-            r=0,
-            b=0,
-            t=30,
-            pad=4
-        ))
-    fig.update_traces(texttemplate='%{text:.2%}', textposition='inside')
-    fig.update_layout(height=490,title=prefix+' Performing Ads by ER(%)',xaxis_range=[df['ER'].min() - (df['ER'].min()/50),df['ER'].max()]) #yaxis_range=[1.2,1.25]
-    config = {
-        "displayModeBar": False
+def getTableCampaignPerf(df):
+    ob = GridOptionsBuilder.from_dataframe(df)
+    ob.configure_column('ORDERNAME', rowGroup=True,hide= True)
+    ob.configure_column('LINE_ITEM', rowGroup=True,hide=True)
+    ob.configure_column('IMPRESSIONS', aggFunc='sum',header_name='IMPRESSIONS',cellRenderer=getNbenderer())
+    ob.configure_column('CLICKS', aggFunc='sum', header_name='CLICKS',cellRenderer=getNbenderer())
+    ob.configure_column('SELLERRESERVEPRICE', aggFunc='sum',cellRenderer=getDollarRenderer(),hide=True)
+    ob.configure_column('CTR', aggFunc='avg',header_name='CTR',cellRenderer= getPercentRenderer())
+    ob.configure_column('CPC', valueGetter=customAggCPC(),header_name='COST PER CLICK',cellRenderer= getDollarRenderer())    
+    ob.configure_grid_options(suppressAggFuncInHeader = True)
+    custom_css = {
+        ".ag-row-level-2 .ag-group-expanded, .ag-row-level-2 .ag-group-contracted":{
+            "display":"none!important",
+        },
+        ".ag-watermark":{
+            "display":"none!important"
+        },
+        ".ag-root-wrapper":{
+             "margin-top":"2px",
+             "border-bottom": "2px",
+             "border-bottom-color": "#b9b5b5",
+             "border-bottom-style": "double"
+             }
+        }
+    gripOption=ob.build()
+    gripOption["autoGroupColumnDef"]= {
+    "headerName": 'CAMPAIGN/AD_TYPE',
+    "cellRendererParams": {
+        "suppressDoubleClickExpand": True,  
+        "suppressCount": True,
+    },
     }
-    st.plotly_chart(fig,config=config, theme="streamlit",use_container_width=True)    
+    AgGrid(df,gripOption, enable_enterprise_modules=True,fit_columns_on_grid_load=True,height=210,custom_css=custom_css,allow_unsafe_jscode=True, update_mode=GridUpdateMode.NO_UPDATE )
+
+
+
+
+
+
+
+
+
+
+def getCTRByGenderByAge(df):
+    return df.groupby(['GENDER','AGE_RANGE']).agg({'CTR':'mean'}).reset_index()
 
 def genSankey(df,cat_cols=[],value_cols='',title='Sankey Diagram'):
     colorPalette = ['red','#646464','#306998','#FFE873','#FFD43B']
@@ -165,84 +245,25 @@ def genSankey(df,cat_cols=[],value_cols='',title='Sankey Diagram'):
     fig = dict(data=[data], layout=layout)
     st.plotly_chart(fig, theme="streamlit",use_container_width=True)
 
-def getEuroRendererCPV():
-    rd = JsCode('''
-        function(params) { 
-            return '<span>$' + parseFloat(params.value).toFixed(2) + '</span>'}
-    ''') 
-    return rd  
-
-def customAggCPV():
-    rd=JsCode('''
-    function(params) {
-                if (params.node.data){
-                    return params.node.data.COSTS/params.node.data.VIEWS;
-                }
-                return params.node.aggData.COSTS/params.node.aggData.VIEWS;
-            }
-    ''')
-    return rd
-
-def customAggCPCV():
-    rd=JsCode('''
-    function(params) {
-                if (params.node.data){
-                    return params.node.data.COSTS/params.node.data.VIDEO_COMPLETIONS;
-                }
-                return params.node.aggData.COSTS/params.node.aggData.VIDEO_COMPLETIONS;
-            }
-    ''')
-    return rd
-
-def getTableCampaignPerf(df):
-    ob = GridOptionsBuilder.from_dataframe(df)
-    ob.configure_column('CAMPAIGN', rowGroup=True,hide= True)
-    ob.configure_column('AD_TYPE', rowGroup=True,hide= True)
-    ob.configure_column('AD_NAME', rowGroup=True,hide= True)
-    ob.configure_column('IMPRESSIONS', aggFunc='sum',header_name='IMPRESSIONS')
-    ob.configure_column('CLICKS', aggFunc='sum', header_name='CLICKS')
-    ob.configure_column('COSTS', aggFunc='sum', hide=True)
-    ob.configure_column('VIEWS', aggFunc='sum', hide=True)
-    ob.configure_column('VIDEO_COMPLETIONS', aggFunc='sum', hide=True)
-    ob.configure_column('CTR', aggFunc='avg',header_name='CTR',cellRenderer= getPercentRenderer())
-    ob.configure_column('CPVMANUAL', valueGetter=customAggCPV(),header_name='COST PER VIDEO VIEW',cellRenderer= getEuroRendererCPV())
-    ob.configure_column('CPCVMANUAL',valueGetter=customAggCPCV(),header_name='COST PER VIDEO COMPLETED',cellRenderer= getEuroRendererCPV())
-    
-    ob.configure_grid_options(suppressAggFuncInHeader = True)
-    custom_css = {
-        ".ag-row-level-2 .ag-group-expanded, .ag-row-level-2 .ag-group-contracted":{
-            "display":"none!important",
-        },
-        ".ag-watermark":{
-            "display":"none!important"
-        },
-        ".ag-root-wrapper":{
-             "margin-top":"28px",
-             "border-bottom": "2px",
-             "border-bottom-color": "#b9b5b5",
-             "border-bottom-style": "double"
-             }
-        }
-    gripOption=ob.build()
-    gripOption["autoGroupColumnDef"]= {
-    "headerName": 'CAMPAIGN/AD_TYPE',
-    "cellRendererParams": {
-        "suppressDoubleClickExpand": True,  
-        "suppressCount": True,
-    },
-    }
-    AgGrid(df,gripOption, enable_enterprise_modules=True,fit_columns_on_grid_load=True,height=342,custom_css=custom_css,allow_unsafe_jscode=True, update_mode=GridUpdateMode.NO_UPDATE )
 
 def getPage(sess):
     global session 
     session = sess
+    advFilter=st.selectbox("Select Advertiser:", getDistinctAdvertisers(),index=0)
+    rawAdvertData=getAdvertiserData(advFilter)
+    rawClicksData=getClickDataByAdvertiser(advFilter)
+    data = [rawAdvertData, rawClicksData]
+    all = pd.concat(data)
+    all['IMPRESSIONS']=all['IMPRESSIONS']*9999
+    all['CLICKS']=all['CLICKS']*9999
+    all['SELLERRESERVEPRICE']=round(all['SELLERRESERVEPRICE']*9999,2)
     # st.subheader("Ads Performance Deep Dive")
     colL,colR,colRR=st.columns([2,1,1])
     with colL:
-        getChartCTRByDevice(getCTRByDevice(getRawCampaign()))
+        getChartCTRByDevice(getCTRByDevice(all))
     with colR:  
-        getChartTopAds(getTopBottomAds(getRawCampaign()))   
+        getChartTopAds(getTopBottomAds(all,bottom=False),asc=True)  
     with colRR:
-        getChartTopAds(getTopBottomAds(getRawCampaign(),bottom=True),asc=False,prefix='Bottom')   
-    getTableCampaignPerf(getKPIByCampaignAds(getRawCampaign()))        
+        getChartTopAds(getTopBottomAds(all,bottom=True),prefix='Bottom',asc=False)   
+    getTableCampaignPerf(getKPIByCampaignAds(all))        
     
