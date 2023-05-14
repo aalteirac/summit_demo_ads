afdeca3f8ac4b43e0c5b4426e2ff0485c9c8670e
Anthony Alteirac <105443206+sfc-gh-aalteirac@users.noreply.github.com>
2023-05-14 19:01:42 +0200

    update 3rd tab, job done

--- whatif.py
+++ whatif.py
@@ -34,45 +34,26 @@ def getClickDataByAdvertiser(adv):
     ''').collect()
     return pd.DataFrame(df)  
 
-def formatBigNumber(number):
-    if number==0.0:
-        return 0
-    units = ['', 'K', 'M', 'G', 'T', 'P']
-    k = 1000.0
-    magnitude = int(floor(log(number, k)))
-    return '%.3f%s' % (number / k**magnitude, units[magnitude])
-
 def getCard(text,val,icon, key,compare=False,titleTextSize="16vw",content_text_size="10vw",unit="%",height='200',iconLeft=90,iconTop=80,backgroundColor='white',progressValue=100,progressColor='green'):
     if compare==False:
         streamlit_kpi(key=key+"_n",height=height,title=text,value=val,icon=icon,progressValue=progressValue,unit=unit,progressColor=progressColor,iconLeft=iconLeft,showProgress=True,iconTop=iconTop,backgroundColor=backgroundColor)
     else:
         streamlit_kpi(key=key+"_n",height=height,title=text,value=val,icon=icon,progressValue=progressValue,unit=unit,iconLeft=iconLeft,showProgress=True,progressColor=progressColor,iconTop=iconTop,backgroundColor=backgroundColor)  
 
-@st.cache_data(show_spinner=False,ttl=5000)
-def getRawCampaign():
-    queryAll=f'''
-    SELECT *,to_date(TO_VARCHAR(DAY, 'yyyy-MM-01')) as MONTH,coalesce((clicks/NULLIF(impressions,0))*100,0) as CTR,coalesce(((clicks + likes + shares)/NULLIF(impressions,0))*100,0) as ER from adverity.adverity."Marketing_Data";
-    '''
-    df = pd.read_sql(queryAll, session)
-    return df
-
 def getTotalCost(df):
     if 'COSTS' in df:
-        return df['COSTS'].sum()
-    if 'COSTS_x' in df:
-        return df['COSTS_x'].sum()    
+        return df['COSTS'].sum()  
 
-
-def getCountrySelectionBox(raw,selec):
-    raw=raw.sort_values(['COUNTRY_NAME'])
+def getCampaignSelectionBox(raw,selec):
+    raw=raw.sort_values(['ORDERNAME'])
     return st.multiselect(
-        'Remove Country to estimate saving:',
-    raw['COUNTRY_NAME'].unique(),default=[],key="country")
+        'Remove Campaign:',
+    raw['ORDERNAME'].unique(),default=[],key="campaign")
 
-def getCampaignSelectionBox(map,selec):
+def getAdsSelectionBox(map,selec):
     return st.multiselect(
-        'Remove Campaign to estimate saving:',
-    map['CAMPAIGN'].unique(),default=[],key='campaign')
+        'Remove Ads:',
+    map['LINE_ITEM'].unique(),default=[],key='ads')
 
 def setShape (x):
     if x == True:
@@ -88,94 +69,99 @@ def getPage(sess):
     rawClicksData=getClickDataByAdvertiser(advFilter)
     data = [rawAdvertData, rawClicksData]
     dt = pd.concat(data)
+    dt=dt.reset_index()
     dt['IMPRESSIONS']=dt['IMPRESSIONS']*9999
     dt['CLICKS']=dt['CLICKS']*9999
     dt['COSTS']=round(dt['SELLERRESERVEPRICE']*9999,2)
     orig=dt.copy()
     dt2=orig.copy()
-    # tab1, tab2 = st.tabs(["Assisted", "Manual"])
+    tab1, tab2 = st.tabs(["Assisted", "Manual"])
 
-    # with tab2:
-    #     countries=st.session_state.get('country') 
-    #     campaings=st.session_state.get('campaign') 
-    #     if countries is None:
-    #         countries=dt['ORDERNAME'].unique()
-    #         campaings=dt['LINE_TIEM'].unique()  
-    #     else:    
-    #         dt=dt[~dt["COUNTRY_NAME"].isin(countries)]
-    #         dt=dt[~dt["CAMPAIGN"].isin(campaings)]
-    #     totalcost=getTotalCost(dt)
-    #     totalcostOrig=getTotalCost(orig)
-    #     compared=(1-((totalcost/totalcostOrig)))*100
-    #     st.subheader("Rebalance Budget Manually"  )
-    #     colL,colR=st.columns(2)
-    #     with colL:
-    #         getCard(text="ORIGINAL COST",val=int(totalcostOrig),icon='fa fa-money-bill',compare=False,key='zero')  
-    #     with colR:
-    #         getCard(text='BUDGET BUFFER:',val=int(totalcostOrig - totalcost), icon='fa fa-piggy-bank',compare=True,key='minusone',unit='$') 
-    #     getCountrySelectionBox(orig,dt) 
-    #     getCampaignSelectionBox(orig,dt)   
+    with tab2:
+        campaings=st.session_state.get('campaign') 
+        ads=st.session_state.get('ads') 
+        if campaings is None:
+            campaings=[]
+            ads=[]
+        else:   
+            if len(campaings)>0: 
+                dt=dt[dt["ORDERNAME"].isin(campaings)]
+            if len(ads)>0:    
+                dt=dt[dt["LINE_ITEM"].isin(ads)] 
+        totalcostOrig=getTotalCost(orig)
+        if len(campaings)>0:
+            save=getTotalCost(dt)
+        else:
+            save=0    
+        compared=(1-((save/totalcostOrig)))*100
+        st.subheader("Rebalance Budget Manually"  )
+        colL,colR=st.columns(2)
+        with colL:
+            getCard(text="ORIGINAL COST",val=int(totalcostOrig),icon='fa fa-money-bill',compare=False,key='zero',unit='$')  
+        with colR:
+            getCard(text='BUDGET BUFFER:',val=int(save), icon='fa fa-piggy-bank',compare=True,key='minusone',unit='$') 
+        getCampaignSelectionBox(orig,dt) 
+        getAdsSelectionBox(orig,dt)   
 
-    # with tab1:
-    clusterSelected=st.session_state.get('clusterstore')
-    if st.session_state.get('clusNum') is not None:
-        kmeans = KMeans(init="random", n_clusters=st.session_state.get('clusNum'), n_init=10, random_state=1)
-    else:
-        kmeans = KMeans(init="random", n_clusters=3, n_init=10, random_state=1)
-    clusterDF=orig.groupby(['LINE_ITEM']).agg({
-                            'IMPRESSIONS':'sum',
-                            'CLICKS':'sum',
-                            'COSTS':'mean'}).reset_index()
-    clusterDF['CTR']=(clusterDF['CLICKS']/clusterDF['IMPRESSIONS'] )*100                                  
-    kmeans.fit(clusterDF[['CTR','COSTS']])  
-    # clusterDF['VIDEO_COMPLETION_RATE']=clusterDF['VIDEO_COMPLETION_RATE']*100
-    clusterDF['CLUSTER'] = kmeans.labels_ 
-    clusterDF['CLUSTER']  = clusterDF['CLUSTER'].astype(str)
-    clusterDF=clusterDF.sort_values(['CLUSTER'])
-    if clusterSelected is not None and  len(clusterSelected)>0:
-        dt2=pd.merge(dt2, clusterDF[~clusterDF['CLUSTER'].isin(list(map(str, clusterSelected)))],on=["LINE_ITEM"])
-        clusterDF['EXCLUDED']= clusterDF['CLUSTER'].isin(list(map(str, clusterSelected)))  
-    totalcost=getTotalCost(dt2)
-    totalcostOrig=getTotalCost(orig)
-    compared=(1-((totalcost/totalcostOrig)))*100
-    st.subheader("Rebalance Budget Scientifically"  )
-    colL,colR=st.columns(2)
-    with colL:
-        getCard(text="ORIGINAL COST",val=int(totalcostOrig),icon='fa fa-money-bill',compare= True,progressColor='transparent',key='one',unit='$')  
-    with colR:
-        getCard(text='BUDGET BUFFER: ',val=int(totalcostOrig - totalcost), icon='fa fa-piggy-bank',compare= True,key='two',unit='$',progressValue=(int(totalcostOrig - totalcost)/int(totalcostOrig))*100) 
-    colL,colR=st.columns(2)   
-    with colL: 
-        st.slider('Cluster Number',2,10,value=3,key='clusNum')
-    with colR:
-        st.multiselect('Exclude Cluster:',np.unique(kmeans.labels_),key='clusterstore')
- 
-    if clusterSelected is not None and  len(clusterSelected)>0:     
-        fig = px.scatter(
-            clusterDF,
-            y="CTR",
-            size="IMPRESSIONS",
-            x='COSTS',
-            color="CLUSTER",
-            symbol = 'EXCLUDED',
-            symbol_map={True:'circle-open',False:'circle'},
-            # symbol = 'EXCLUDED',
-            # symbol_sequence= [ 'circle-open','circle'],
-            hover_name="LINE_ITEM",
-            size_max=30,
-            height=430
-        ) 
-    else:
-        fig = px.scatter(
-            clusterDF,
-            y="CTR",
-            size="IMPRESSIONS",
-            x='COSTS',
-            color="CLUSTER",
-            hover_name="LINE_ITEM",
-            size_max=30,
-            height=400
-        )
-    fig.update_layout(xaxis={'visible': True, 'showticklabels': False})    
-    st.subheader("Clustering Ads by IMPRESSIONS and CTR"  )      
-    st.plotly_chart(fig, theme="streamlit",use_container_width=True)   
+    with tab1:
+        clusterSelected=st.session_state.get('clusterstore')
+        if st.session_state.get('clusNum') is not None:
+            kmeans = KMeans(init="random", n_clusters=st.session_state.get('clusNum'), n_init=10, random_state=1)
+        else:
+            kmeans = KMeans(init="random", n_clusters=3, n_init=10, random_state=1)
+        clusterDF=orig.groupby(['LINE_ITEM']).agg({
+                                'IMPRESSIONS':'sum',
+                                'CLICKS':'sum',
+                                'COSTS':'mean'}).reset_index()
+        clusterDF['CTR']=(clusterDF['CLICKS']/clusterDF['IMPRESSIONS'] )*100                                  
+        kmeans.fit(clusterDF[['CTR','COSTS']])  
+        clusterDF['CLUSTER'] = kmeans.labels_ 
+        clusterDF['CLUSTER']  = clusterDF['CLUSTER'].astype(str)
+        clusterDF=clusterDF.sort_values(['CLUSTER'])
+        if clusterSelected is not None and  len(clusterSelected)>0:
+            dt2=pd.merge(dt2, clusterDF[~clusterDF['CLUSTER'].isin(list(map(str, clusterSelected)))],on=["LINE_ITEM"])
+            clusterDF['EXCLUDED']= clusterDF['CLUSTER'].isin(list(map(str, clusterSelected)))  
+        totalcost=getTotalCost(dt2)
+        totalcostOrig=getTotalCost(orig)
+        compared=(1-((totalcost/totalcostOrig)))*100
+        st.subheader("Rebalance Budget Scientifically"  )
+        colL,colR=st.columns(2)
+        with colL:
+            getCard(text="ORIGINAL COST",val=int(totalcostOrig),icon='fa fa-money-bill',compare= True,progressColor='transparent',key='one',unit='$')  
+        with colR:
+            getCard(text='BUDGET BUFFER: ',val=int(totalcostOrig - totalcost), icon='fa fa-piggy-bank',compare= True,key='two',unit='$',progressValue=(int(totalcostOrig - totalcost)/int(totalcostOrig))*100) 
+        colL,colR=st.columns(2)   
+        with colL: 
+            st.slider('Cluster Number',2,10,value=3,key='clusNum')
+        with colR:
+            st.multiselect('Exclude Cluster:',np.unique(kmeans.labels_),key='clusterstore')
+    
+        if clusterSelected is not None and  len(clusterSelected)>0:     
+            fig = px.scatter(
+                clusterDF,
+                y="CTR",
+                size="IMPRESSIONS",
+                x='COSTS',
+                color="CLUSTER",
+                symbol = 'EXCLUDED',
+                symbol_map={True:'circle-open',False:'circle'},
+                # symbol = 'EXCLUDED',
+                # symbol_sequence= [ 'circle-open','circle'],
+                hover_name="LINE_ITEM",
+                size_max=30,
+                height=430
+            ) 
+        else:
+            fig = px.scatter(
+                clusterDF,
+                y="CTR",
+                size="IMPRESSIONS",
+                x='COSTS',
+                color="CLUSTER",
+                hover_name="LINE_ITEM",
+                size_max=30,
+                height=400
+            )
+        fig.update_layout(xaxis={'visible': True, 'showticklabels': False})    
+        st.subheader("Clustering Ads by IMPRESSIONS and CTR"  )      
+        st.plotly_chart(fig, theme="streamlit",use_container_width=True)   
